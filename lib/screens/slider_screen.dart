4a52eb3a4211d2c70a65e1ec7bcea37507f32aba
Jared Tc <75559684+JaredTc@users.noreply.github.com>
2022-07-16 15:20:14 -0500

    Sliders Widgets two

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -44,7 +44,7 @@ class _SliderScreenState extends State<SliderScreen> {
           SwitchListTile(value: _sliderEnabled,
           activeColor: AppTheme.primary,
           title: const Text('Enabled Slider'), 
-          onChanged: ( value ) => setState((){ _sliderEnabled = value ?? true; }
+          onChanged: ( value ) => setState((){ _sliderEnabled = value; }
           )
           ),
           Expanded(
